5503fa0e1377f35b0f928dda8ac754b269b9c592
edisontrent1337 <sinthu1337@gmail.com>
2016-01-08 18:42:01 +0100

    pop up feed edited

--- core/src/com/trent/awesomejumper/engine/modelcomponents/popups/PopUpFeed.java
+++ core/src/com/trent/awesomejumper/engine/modelcomponents/popups/PopUpFeed.java
@@ -40,6 +40,7 @@ public class PopUpFeed extends ModelComponent {
 
 
     }
+    
 
     // CONSTRUCTOR
     // ---------------------------------------------------------------------------------------------
