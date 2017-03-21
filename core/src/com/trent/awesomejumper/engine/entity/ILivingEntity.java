9053b382e4741ab2a5f4e7495389147379c84bcd
edisontrent1337 <sinthu1337@gmail.com>
2017-03-21 14:55:10 +0100

    changed body api

---
+++ core/src/com/trent/awesomejumper/engine/entity/ILivingEntity.java
@@ -0,0 +1,15 @@
+package com.trent.awesomejumper.engine.entity;
+
+import com.trent.awesomejumper.engine.modelcomponents.Body;
+import com.trent.awesomejumper.engine.modelcomponents.Health;
+
+/**
+ * Created by Sinthu on 24.08.2016.
+ */
+public interface LivingEntity {
+
+
+    Body getBody();
+    Health getHealth();
+
+}
